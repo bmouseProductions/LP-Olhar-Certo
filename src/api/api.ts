22b96fbf07218cc6d78ea28ced8514c702b7e298
bmouseProductions <137824606+bmouseProductions@users.noreply.github.com>
2023-07-21 09:20:55 -0300

    Update api.ts

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export const api = axios.create({
-    baseURL: "https://backend-giolaser.onrender.com"
+    baseURL: "https://backend-olhar-certo.onrender.com"
 })
 
 interface propsFormData {
